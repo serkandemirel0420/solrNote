78c9b161e05267c21e78f41bb260f52e0ed1b689
serkan <serkandemirel0420@gmail.com>
2020-01-18 03:17:38 +0300

    v7

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -5,7 +5,43 @@ import App from './App'
 import router from './router'
 import store from './store'
 
-if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
+const {autoUpdater} = require("electron-updater");
+const isDev = require("electron-is-dev");
+autoUpdater.logger = require("electron-log");
+autoUpdater.logger.transports.file.level = "info";
+
+autoUpdater.on('checkig-for-update', () => {
+  console.log("checking for updates...");
+});
+
+autoUpdater.on('update-available', (info) => {
+  console.log("update available");
+  console.log("Version", info.version);
+  console.log("Release Date", info.releaseDate);
+});
+
+autoUpdater.on('update-not-available', () => {
+  console.log("Update not available");
+});
+
+autoUpdater.on('download-progress', (progress) => {
+  console.log(`Progress ${Math.floor(progress.percent)}`);
+});
+
+autoUpdater.on('update-downloaded', (info) => {
+  console.log("Update downloaded");
+  autoUpdater.quitAndInstall()
+});
+
+autoUpdater.on('error', (error) => {
+  console.log("Error occured",error);
+});
+
+if (!isDev) {
+  autoUpdater.checkForUpdates()
+}
+
+if (!process.env.IS_WEB) {}Vue.use(require('vue-electron'));
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
